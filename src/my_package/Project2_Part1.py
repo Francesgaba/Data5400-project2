b221c7798d64fb40736dc6ad999bb42005b782df
Francesgaba <146161347+Francesgaba@users.noreply.github.com>
2023-12-05 15:51:46 -0500

    Update Project2_Part1.py

--- src/my_package/Project2_Part1.py
+++ src/my_package/Project2_Part1.py
@@ -13,16 +13,39 @@ import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
 
+import requests
+import xml.etree.ElementTree as ET
+import pandas as pd
+
 class SitemapParser:
+    """
+    A parser for extracting sitemap URLs from a robots.txt file and parsing the sitemaps to collect URLs.
+    
+    Attributes:
+        robots_url (str): The URL of the robots.txt file.
+        sitemaps (list): A list of sitemap URLs found in the robots.txt file.
+    """
+
     def __init__(self, robots_url):
-        self.robots_url = robots_url  # URL of the robots.txt file
-        self.sitemaps = self.get_sitemaps()  # List of sitemap URLs found in the robots.txt
+        """
+        Initializes the SitemapParser with a given robots.txt URL.
+
+        Args:
+            robots_url (str): The URL of the robots.txt file.
+        """
+        self.robots_url = robots_url
+        self.sitemaps = self.get_sitemaps()
 
     def get_sitemaps(self):
+        """
+        Retrieves and parses the robots.txt file to extract sitemap URLs.
+
+        Returns:
+            list: A list of sitemap URLs.
+        """
         try:
             response = requests.get(self.robots_url)
             response.raise_for_status()
-            # splitting the text into lines and filtering for those that start with 'Sitemap:'
             sitemaps = [line.split(' ')[1] for line in response.text.split('\n') if line.startswith('Sitemap:')]
             return sitemaps
         except requests.RequestException as e:
@@ -30,15 +53,22 @@ class SitemapParser:
             return []
 
     def parse_sitemap(self, sitemap_url):
+        """
+        Parses a given sitemap URL and extracts URLs from it.
+
+        Args:
+            sitemap_url (str): The URL of the sitemap to parse.
+
+        Returns:
+            list: A list of dictionaries containing the parsed URL data.
+        """
         try:
-            response = requests.get(sitemap_url)  # Fetching the sitemap
-            response.raise_for_status()  # raise an exception for HTTP errors
-            root = ET.fromstring(response.content)  # parsing XML content
-            # Handling namespaces in XML
+            response = requests.get(sitemap_url)
+            response.raise_for_status()
+            root = ET.fromstring(response.content)
             namespace = '{' + root.tag.split('}')[0].strip('{') + '}'
             urls = []
             for sitemap in root.findall('.//' + namespace + 'url'):
-                # extracting URL information and removing namespace from tags
                 url_info = {child.tag[len(namespace):]: child.text for child in sitemap}
                 urls.append(url_info)
             return urls
@@ -47,11 +77,17 @@ class SitemapParser:
             return []
 
     def dataframe(self):
+        """
+        Converts the parsed sitemap URLs into a Pandas DataFrame.
+
+        Returns:
+            DataFrame: A DataFrame containing all URLs from the parsed sitemaps.
+        """
         all_urls = []
         for sitemap_url in self.sitemaps:
-            urls = self.parse_sitemap(sitemap_url)  # Parsing each sitemap
-            all_urls.extend(urls)  # Aggregating all URLs
-        return pd.DataFrame(all_urls)  
+            urls = self.parse_sitemap(sitemap_url)
+            all_urls.extend(urls)
+        return pd.DataFrame(all_urls)
 
 parser = SitemapParser('https://www.nytimes.com/robots.txt') 
 df = parser.dataframe()  # Converting the parsed sitemap to a DataFrame
